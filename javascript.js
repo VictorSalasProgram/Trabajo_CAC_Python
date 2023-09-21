688c664925e9c9f4a41e8d1694281a93b5a4fa5f
Victor <victorsalasok@gmail.com>
2023-09-21 10:18:23 -0300

    validacion de formulario

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
 let botonCalcular = document.getElementById("boton-calcular");
+let formNombre = document.getElementById("form-name")
+let formCorreo = document.getElementById("form-correo")
 botonCalcular.onclick= popCalcular;
 
 function popCalcular(){
-    alert("Calculando cotizacion")
+    if(formNombre.value != ""){
+        if(formCorreo.value != ""){
+            alert("Calculando cotizacion")
+        }
+        else{
+            alert("INGRESE CORREO")
+        }
+        
+    }
+    else{
+        alert("INGRESE NOMBRE")
+    }
 }
