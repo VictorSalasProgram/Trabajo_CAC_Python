6d72f0206cb9e80b6718b9300b204293e8aa3ce5
Victor <victorsalasok@gmail.com>
2023-09-21 23:59:47 -0300

    validacion terminos y condiciones, pagina de terminos

--- javascript.js
+++ javascript.js
@@ -4,17 +4,25 @@ let formNombre = document.getElementById("form-name")
 let formCorreo = document.getElementById("form-correo")
 let formServicio1 = document.getElementById("select")
 let formServicio2 = document.getElementById("select2")
+let formCheck = document.getElementById("flexCheckDefault")
 botonCalcular.onclick= popCalcular;
 botonBorrar.onclick = borrarCampos;
 
 function popCalcular(){
     if(formNombre.value != ""){
         if(formCorreo.value != ""){
-            formNombre.setAttribute("disabled", "true")
-            formCorreo.setAttribute("disabled", "true")
-            formServicio1.setAttribute("disabled", "true")
-            formServicio2.setAttribute("disabled", "true")
-            alert("Calculando cotizacion")
+            if(formCheck.checked === true){
+                formNombre.setAttribute("disabled", "true")
+                formCorreo.setAttribute("disabled", "true")
+                formServicio1.setAttribute("disabled", "true")
+                formServicio2.setAttribute("disabled", "true")
+                formCheck.setAttribute("disabled", "true")
+                alert("Calculando cotizacion")
+            }
+            else{
+                alert("ACEPTE LOS TERMINOS Y CONDICIONES")
+            }
+          
             
         }
         else{
@@ -34,4 +42,5 @@ function borrarCampos(){
     formNombre.removeAttribute("disabled")
     formServicio1.removeAttribute("disabled")
     formServicio2.removeAttribute("disabled")
+    formCheck.removeAttribute("disabled")
 }
